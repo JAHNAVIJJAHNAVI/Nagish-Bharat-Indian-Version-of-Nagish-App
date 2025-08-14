b1d7aab9ef1548f93e84b7433a54342ac2d23488
P.JAHNAVI <161014844+JAHNAVIJJAHNAVI@users.noreply.github.com>
2025-08-14 12:19:42 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Backend is running!"
+    return jsonify({"message": "Nagish-Bharat Backend Running!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
